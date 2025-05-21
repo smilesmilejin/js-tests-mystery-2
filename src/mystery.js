d4ff0bca50384b1dc5df883080d68e8e1f95fe0e
Xinshuang Jin <jinxinshuang@gmail.com>
2025-05-21 14:08:39 -0400

    2025/05/21 Activity

--- src/mystery.js
+++ src/mystery.js
@@ -1,1 +1,9 @@
-export const mystery2 = (num) => {};
+export const mystery2 = (num) => {
+  // dividable by 4 and 400 return true
+  // dividable by 100 return false
+  // if it is dividalbe by 100, it is dividable by 100
+  if (num % 400 === 0 || (num % 4 === 0 && num % 100 !== 0)) {
+    return true;
+  }
+  return false;
+};
