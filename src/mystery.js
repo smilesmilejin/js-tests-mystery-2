b74a6cc7a1c4f17abb796b44518c12755bb7f782
Xinshuang Jin <jinxinshuang@gmail.com>
2025-05-21 14:32:18 -0400

    2025/05/21 activity update

--- src/mystery.js
+++ src/mystery.js
@@ -1,8 +1,18 @@
+// export const mystery2 = (num) => {
+//   // dividable by 4 and 400 return true
+//   // dividable by 100 return false
+//   // if it is dividalbe by 100, it is dividable by 100
+//   if (num % 400 === 0 || (num % 4 === 0 && num % 100 !== 0)) {
+//     return true;
+//   }
+//   return false;
+// };
+
+// leap year
 export const mystery2 = (num) => {
-  // dividable by 4 and 400 return true
-  // dividable by 100 return false
-  // if it is dividalbe by 100, it is dividable by 100
-  if (num % 400 === 0 || (num % 4 === 0 && num % 100 !== 0)) {
+  if (num % 400 === 0) {
+    return true;
+  } else if (num % 4 === 0 && num % 100 !== 0) {
     return true;
   }
   return false;
